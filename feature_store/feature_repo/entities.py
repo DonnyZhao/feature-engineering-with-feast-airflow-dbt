d2ad838d5d6474a89cddbfbd77dc3b1e7303dd2f
Lawrence Liu <lwrncliu@gmail.com>
2023-10-23 16:00:29 -0400

    set entity join key dtype

--- feature_store/feature_repo/entities.py
+++ feature_store/feature_repo/entities.py
@@ -3,5 +3,6 @@ from feast import Entity
 location_entity = Entity(
     name="location",
     join_keys=["LOCATION_ID"],
+    value_type=ValueType.INT32,
     description="a primary key to fetch location features"
 )
